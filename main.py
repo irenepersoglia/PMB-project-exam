aa730bb6fd992b737c3a24377d5a01d0bcf8d32a
irenepersoglia <irene.persoglia@gmail.it>
2023-05-22 19:05:42 +0200

    Save option added

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     argparser.add_argument('--dictionary', type = str, help = 'set the dictionary for the translation', default = "", required = True)
     argparser.add_argument('--table', type = str, help = 'set the table to translate', default = "", required = True)
     argparser.add_argument('--num_processes', type = int, help = 'set the number of processes', default = 1, required = False)
+    argparser.add_argument('--save_table', type = str, help = 'choose to overwrite the file (yes or no)', default = "", required = True)
 
 
     # Parameters
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     dictionary_path = args.dictionary
     table_path = args.table
     n_processes = args.num_processes
+    save_table = args.save_table
     
     # Take columns names (languages) from the dictionary
     with open(dictionary_path, "r") as file:
@@ -40,5 +42,6 @@ if __name__ == "__main__":
     for column in columns_to_translate:
         translator.translate_table(table_df, column, "display name")
 
-    
-    
+    # Save the new table in the same path
+    if save_table == 'yes':
+        table_df.to_csv(table_path, index = False)
