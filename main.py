bc2e58be79453f68c173e8ca71b56abaa76bd9de
irenepersoglia <irene.persoglia@gmail.it>
2023-05-25 16:30:02 +0200

    update saving option

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     argparser.add_argument('--dictionary', type = str, help = 'set the dictionary for the translation', default = "", required = True)
     argparser.add_argument('--table', type = str, help = 'set the table to translate', default = "", required = True)
     argparser.add_argument('--num_processes', type = int, help = 'set the number of processes', default = 1, required = False)
-    argparser.add_argument('--save_table', type = str, help = 'choose to overwrite the file (yes or no)', default = "", required = True)
+    argparser.add_argument('--save_table', help = 'choose to save the translated file (True or False)', default = False, required = False, action="store_true")
 
 
     # Parameters
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     
     # Load table to translate
     table_df = pd.read_csv(table_path, sep = " ")
-    table_df[2] = np.dot(table_df[2], 0.001)
+    table_df["combined_score"] = np.dot(table_df["combined_score"], 0.001)
     
     # Translate table
     columns_to_translate = [col for col in table_df.columns if "protein" in col]
@@ -45,7 +45,9 @@ if __name__ == "__main__":
         translator.translate_table(table_df, column, "display name")
 
     # Save the new table in the same path
-    if save_table == 'yes':
-        table_df.to_csv(table_path, index = False)
-        
-    
+    if save_table:
+        translated_table_file_name = "translated_" + table_path.split("/")[-1]
+        translated_table_path = "/".join(table_path.split("/")[:-1])
+        translated_table_path += "/"
+        translated_table_path += translated_table_file_name
+        table_df.to_csv(translated_table_path, index = False)
